06c285cae60b1b74406b88884579c16ecdce92b6
wojteke <wojteksz1999@gmail.com>
2022-10-28 22:28:22 +0200

    feat(lib): Added Union-Find algorithm and basic linear algebra operations

--- src/oofal.cpp
+++ src/oofal.cpp
@@ -2,6 +2,172 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+
+#pragma region LinearAlgebra
+
+template <typename T>
+std::vector<T> crossProduct3D(const std::vector<T>& v1, const std::vector<T>& v2) {
+	std::vector<T> result(v1.size());
+	result[0] = v1[1] * v2[2] - v1[2] * v2[1];
+	result[1] = v1[2] * v2[0] - v1[0] * v2[2];
+	result[2] = v1[0] * v2[1] - v1[1] * v2[0];
+	return result;
+}
+
+template <typename T>
+T dotProduct(const std::vector<T>& v1, const std::vector<T>& v2) {
+	T result = 0;
+	for (size_t i = 0; i < v1.size(); i++)
+	{
+		result += v1[i] * v2[i];
+	}
+	return result;
+}
+
+template <typename T>
+float lengthf(const std::vector<T>& vec) {
+	auto result = dotProduct(vec, vec);
+	return sqrtf(result);
+}
+
+template <typename T>
+double length(const std::vector<T>& vec) {
+	auto result = dotProduct(vec, vec);
+	return sqrt(result);
+}
+
+#pragma endregion
+
+#pragma region Sets
+/// <summary>
+/// Basic idea of union-find, can be painfull to use effectively 
+/// TODO: Improve this in the future
+/// </summary>
+struct UnionFindSet {
+	struct Node {
+		int parent;
+		int rank;
+	};
+
+	std::vector<Node> set;
+
+	int find(int index) {
+		if (set[index].parent == index) {
+			return index;
+		}
+		set[index].parent = find(set[index].parent);
+		return set[index].parent;
+	}
+
+	void union_(int index1, int index2) {
+		if (index1 >= set.size()) set.push_back({ index1, 0 });
+		if (index2 >= set.size()) set.push_back({ index1, 0 });
+
+		int set1 = find(index1);
+		int set2 = find(index2);
+
+		if (set1 == set2) return;
+
+		if (set[set1].rank >= set[set2].rank) {
+			set[set1].rank++;
+
+			set[set2].parent = set1;
+			set[set2].rank = 0;
+		}
+		else {
+			set[set2].rank++;
+
+			set[set1].parent = set2;
+			set[set1].rank = 0;
+		}
+	}
+};
+
+/// <summary>
+/// Use this if you need to use custom data as your set's key
+/// </summary>
+/// <typeparam name="T"></typeparam>
+template <typename T>
+struct UnionFindMap {
+	struct Node {
+		int index;
+		int parent;
+		int rank;
+	};
+
+	std::unordered_map<T, Node> map;
+	std::vector<Node> set;
+
+	int find(int index) {
+		if (set[index].parent == index) {
+			return index;
+		}
+		set[index].parent = find(set[index].parent);
+		return set[index].parent;
+	}
+
+	int findByValue(T val) {
+		if (map.count(val) <= 0) return -1;
+
+		return find(map[val].index);
+	}
+
+	void union_(int index1, int index2) {
+		int set1 = find(index1);
+		int set2 = find(index2);
+
+		if (set1 == set2) return;
+
+		if (set[set1].rank >= set[set2].rank) {
+			set[set1].rank++;
+
+			set[set2].parent = set1;
+			set[set2].rank = 0;
+		}
+		else {
+			set[set2].rank++;
+
+			set[set1].parent = set2;
+			set[set1].rank = 0;
+		}
+	}
+
+	void unionByValue(T val1, T val2) {
+		auto val1Exists = map.count(val1) > 0;
+		auto indexVal1 = -1;
+		if (!val1Exists) {
+			indexVal1 = set.size();
+			Node node = { indexVal1, indexVal1, 0 };
+			set.push_back(node);
+			map[val1] = node;
+		}
+		else {
+			indexVal1 = map[val1].index;
+		}
+
+		auto val2Exists = map.count(val2) > 0;
+		auto indexVal2 = -1;
+		if (!val2Exists) {
+			indexVal2 = set.size();
+			Node node = { indexVal2, indexVal2, 0 };
+			set.push_back(node);
+			map[val2] = node;
+		}
+		else {
+			indexVal2 = map[val2].index;
+		}
+
+		union_(indexVal1, indexVal2);
+	}
+};
+
+#pragma endregion
+
+
 
 #pragma region Graphs
 
@@ -10,10 +176,6 @@ struct Edge {
 	int startIndex;
 	int endIndex;
 	E value;
-
-	bool operator<(const Edge& rhs) {
-		return value < rhs.value;
-	}
 };
 
 template <typename V, typename E>
@@ -458,18 +620,27 @@ GraphCharacteristics computeCharacteristics(IGraph<V, E>* graph) {
 
 #pragma region MinimalSpanningTrees
 
+template <typename E>
+struct MinimalSpanningTree {
+	std::vector<Edge<E>> edges;
+	E edgesSum;
+	bool graphDisconnected;
+};
+
 template <typename V, typename E>
-std::vector<Edge<E>> Prim(IGraph<V, E>* graph) {
+MinimalSpanningTree<E> MST_Prim(IGraph<V, E>* graph) {
 	auto count = graph->size();
+	E sum = 0;
 	std::vector<Edge<E>> tree;
 	tree.reserve(count - 1);
 
-	std::vector<bool> visited(count);
-
-	std::priority_queue<Edge<E>> pQueue;
+	std::vector<bool> visited(count, false);
+	auto compare = [](Edge<E> const& a, Edge<E> const& b) { return a.value > b.value; };
+	std::priority_queue<Edge<E>, std::vector<Edge<E>>, decltype(compare)> pQueue(compare);
 	int vertexIndex = 0;
+	visited[vertexIndex] = true;
 
-	for (int i = 0; i < count; i++)
+	for (int i = 0; i < count - 1; i++)
 	{
 		auto edges = graph->getNeighboursEdges(vertexIndex);
 		for (const auto& edge : edges) {
@@ -481,11 +652,80 @@ std::vector<Edge<E>> Prim(IGraph<V, E>* graph) {
 		do {
 			edge = pQueue.top();
 			pQueue.pop();
-		} while (!visited[edge.endIndex]);
+		} while (visited[edge.endIndex] && !pQueue.empty());
+
+		if (visited[edge.endIndex]) {
+			return { std::vector<Edge<E>>(), 0, true};
+		}
+
 		visited[edge.endIndex] = true;
 		tree.push_back(edge);
+		sum += edge.value;
 		vertexIndex = edge.endIndex;
 	}
+	return { tree, sum, false };
 }
 
 #pragma endregion
+
+
+template <typename E>
+struct by_v1 {
+	bool operator()(Edge<E> const& a, Edge<E> const& b) const {
+		return a.startIndex < b.startIndex;
+	}
+};
+
+
+void UnionFindSolution() {
+	char operation;
+	std::string IP1_string = "", IP2_string = "";
+	UnionFindMap<long long> unionFind;
+	while (std::cin >> operation) {
+		std::cin >> IP1_string >> IP2_string;
+
+		IP1_string.erase(remove(IP1_string.begin(), IP1_string.end(), '.'), IP1_string.end());
+		IP2_string.erase(remove(IP2_string.begin(), IP2_string.end(), '.'), IP2_string.end());
+		long long IP1 = stoll(IP1_string);
+		long long IP2 = stoll(IP2_string);
+		if (operation == 'T') {
+			auto set1 = unionFind.findByValue(IP1);
+			auto set2 = unionFind.findByValue(IP2);
+			std::cout << (set1 != -1 && set2 != -1 && set1 == set2 ? "T" : "N") << "\n";
+		}
+		else {
+			unionFind.unionByValue(IP1, IP2);
+		}
+	}
+}
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	// std::cout.tie(NULL);
+
+	UnionFindSolution();
+	/*int tests = 0;
+	std::cin >> tests;
+	while (tests--) {
+		int vertices = 0, edges = 0;
+		std::cin >> vertices >> edges;
+		auto graph = new AdjentencyGraph<int, int>(vertices, 0);
+
+		for (int i = 0; i < edges; i++)
+		{
+			int v1, v2, value;
+			std::cin >> v1 >> v2 >> value;
+			graph->addEdgeBothWays(v1-1, v2-1, value);
+		}
+
+
+		auto tree = MST_Prim(graph);
+		if (tree.graphDisconnected) std::cout << "brak\n";
+		else std::cout << tree.edgesSum << "\n";
+
+		delete graph;
+	}*/
+	// _getch();
+}
